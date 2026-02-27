85f3713493d55df76e73e8b77a610601d3c51a74
prabhath <prabhath.amila@gmail.com>
2026-02-28 02:10:57 +0530

    chore: setup shadcn button

--- components/todo-list.tsx
+++ components/todo-list.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { Button } from "./ui/button";
+import { ArrowUpIcon, ArrowDownIcon } from "lucide-react";
 export default function TodoList() {
     return (
         <>
@@ -15,6 +17,21 @@ export default function TodoList() {
                 <li>Rehearse a movie scene</li>
                 <li>Improve the spectrum technology</li>
             </ul>
+            <div className="flex gap-4">
+                <Button className="bg-black capitalize text-lg font-bold">Learn more</Button>
+                <Button variant="outline">Click Here</Button>
+                <Button variant="outline" size="icon" area-label="Submit">
+                    <ArrowUpIcon />
+                </Button>
+                <Button variant="destructive">Click Here</Button>
+                <Button variant="secondary">Click Here</Button>
+                <Button variant="ghost">Click Here</Button>
+                <Button variant="link">Click Here</Button>
+                <Button variant="outline" size="icon" area-label="Submit">
+                    <ArrowDownIcon />
+                </Button>
+
+            </div>
         </>
     );
 }
